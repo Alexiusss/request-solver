de2defa76e113ab5b5fa81320a8d3bd12bcbf8a8
Alexey Boyarinov <boyarinovalexey@gmail.com>
2024-10-21 18:07:51 +0300

    Init

---
+++ src/main/java/com/example/request_solver/RequestSolverApplication.java
@@ -0,0 +1,13 @@
+package com.example.request_solver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestSolverApplication.class, args);
+	}
+
+}
