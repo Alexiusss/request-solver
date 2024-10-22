20171923bfda9c92c5978c1719a32f1f46af7f01
Alexey Boyarinov <boyarinovalexey@gmail.com>
2024-10-22 18:33:51 +0300

    Implemented tests for user controller

--- src/test/java/com/example/request_solver/web/UserControllerTest.java
+++ src/test/java/com/example/request_solver/web/UserControllerTest.java
@@ -1,37 +1,54 @@
 package com.example.request_solver.web;
 
+import com.example.request_solver.model.Role;
+import com.example.request_solver.service.UserService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static com.example.request_solver.util.UserTestData.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class UserControllerTest extends AbstractControllerTest {
 
     public static final String REST_URL = UserController.REST_URL;
 
+    @Autowired
+    private UserService userService;
+
     @Test
     @WithMockUser(roles = {"ADMIN"})
     void getAllUsers() throws Exception {
         perform(MockMvcRequestBuilders.get(REST_URL))
                 .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(USER_LIST)))
                 .andExpect(status().isOk());
     }
 
     @Test
     @WithMockUser(roles = {"ADMIN"})
     void getByName() throws Exception {
-        perform(MockMvcRequestBuilders.get(REST_URL + "/test" + "/name"))
+        perform(MockMvcRequestBuilders.get(REST_URL + "/" + USER_NAME + "/name"))
                 .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(USER_TO)))
                 .andExpect(status().isOk());
     }
 
     @Test
     @WithMockUser(roles = {"ADMIN"})
     void setOperatorRole() throws Exception {
-        perform(MockMvcRequestBuilders.patch(REST_URL + "/1000000" + "/role"))
-                .andDo(print())
-                .andExpect(status().isOk());
+        perform(MockMvcRequestBuilders.patch(REST_URL + "/" + USER_ID + "/role")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        boolean isRoleAdded = userService.get(USER_ID).getRoles().contains(Role.OPERATOR);
+        Assertions.assertThat(isRoleAdded).isTrue();
     }
 }
